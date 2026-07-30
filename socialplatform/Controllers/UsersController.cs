ee0cc57485f55292880af3d09cdd3c06081c7e44
ervaerdgn <ervaerdgn6@gmail.com>
2026-07-30 14:51:01 +0300

    UsersController added for user login

---
+++ socialplatform/Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using socialplatform.Models;
+
+namespace socialplatform.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private static List<User> kullanicilar = new List<User>();
+
+        [HttpGet]
+        public ActionResult<List<User>> GetAll()
+        {
+            return kullanicilar;
+        }
+
+        [HttpPost]
+        public ActionResult<User> Create(User yeniKullanici)
+        {
+            yeniKullanici.Id = kullanicilar.Count + 1;
+            kullanicilar.Add(yeniKullanici);
+            return Ok(yeniKullanici);
+        }
+    }
+}
---
+++ socialplatform/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace socialplatform.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; }= string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public DateTime Time {  get; set; }= DateTime.Now;
+      
+        
+    }
+}
