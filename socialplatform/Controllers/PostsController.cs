e11432d0219c337dfae01ccd47a56bc09988139d
ervaerdgn <ervaerdgn6@gmail.com>
2026-08-07 18:59:44 +0300

    Add feed endpoint to fetch posts from followed users

--- socialplatform/Controllers/PostsController.cs
+++ socialplatform/Controllers/PostsController.cs
@@ -19,7 +19,24 @@ namespace socialplatform.Controllers
         [HttpGet]
         public async Task<ActionResult<List<Post>>> GetAll()
         {
+
             return await _context.Posts.Include(p => p.User).ToListAsync();
+        }
+        [HttpGet("feed/{userId}")]
+        public async Task<ActionResult<List<Post>>> GetFeed(int userId)
+        {
+            var takipedilenler = await _context.Follows
+                .Where(f => f.FollowerID == userId)
+                .Select(f => f.FollowingID)
+                .ToListAsync();
+
+            var feed = await _context.Posts
+                .Where(p => takipedilenler.Contains(p.UserID))
+                .Include(p => p.User)
+                .OrderByDescending(p => p.Zaman)
+                .ToListAsync();
+
+            return feed;
         }
 
         [HttpPost]
--- socialplatform/Models/Follow.cs
+++ socialplatform/Models/Follow.cs
@@ -3,7 +3,7 @@
     public class Follow
     {
         public int Id { get; set; }
-        public DateTime time { get; set; }= DateTime.Now;
+        public DateTime zaman { get; set; }= DateTime.Now;
         public int FollowerID { get; set; } //takip eden kişi olarak düşün
         public User? Follower { get; set; }
         public int FollowingID { get; set; } //takip edilen kişi olarak 
